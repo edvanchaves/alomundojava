133173d37a36d0e66321291fa86df0af5afc4207
edvanchaves <edvanchaves@gmail.com>
2017-03-22 09:01:56 -0300

    Criacao dos arquivos de teste do Scanner

---
+++ TesteScanner2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TesteScanner{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite sua idade: ");
+		int idade = sc.nextInt();
+
+		System.out.println("Sua idade é :" + idade + " anos.");
+	}
+}
