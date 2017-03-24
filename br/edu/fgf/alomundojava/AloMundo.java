e5c805b650eb86caef361ce0edbeed9b32be35ee
Isael Sousa <faelp22@gmail.com>
2017-03-24 20:10:19 -0300

    teste

--- br/edu/fgf/alomundojava/AloMundo.java
+++ br/edu/fgf/alomundojava/AloMundo.java
@@ -1,5 +1,7 @@
 package br.edu.fgf.alomundojava;
+
 public class AloMundo {
+    
     public static void main (String[] args) {
 	//imprime na saida do terminal
         System.out.println("Alo mundo FGF... Teste");
